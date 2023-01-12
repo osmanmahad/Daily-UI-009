0c1965af8109de7bd0377ebe230ed6897e1e97a3
osmanmahad <maosman@uwaterloo.ca>
2023-01-11 20:34:59 -0500

    commit

---
+++ js/playlist.js
@@ -0,0 +1,32 @@
+let songs = [
+  {
+    name: "Anti-Hero",
+    artist: "Taylor Swift",
+    img: "img1",
+    audio: "music1",
+  },
+  {
+    name: "Unholy",
+    artist: "Sam Smith and Kim Petras",
+    img: "img2",
+    audio: "music2",
+  },
+  {
+    name: "Kill Bill",
+    artist: "SZA",
+    img: "img3",
+    audio: "music3",
+  },
+  {
+    name: "Rich Flex",
+    artist: "Drake and 21 Savage ",
+    img: "img4",
+    audio: "music4",
+  },
+  {
+    name: "Creepin' ",
+    artist: "Metro Boomin, The Weeknd & 21 Savage",
+    img: "img5",
+    audio: "music5",
+  },
+];
